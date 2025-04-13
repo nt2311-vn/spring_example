007a794d0393e8ac0a8953be6e59ae842518d2d2
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-13 21:47:48 +0700

    using beam to manage custom class in spring boot application

--- src/main/java/com/nt2311/example/ExampleApplication.java
+++ src/main/java/com/nt2311/example/ExampleApplication.java
@@ -2,14 +2,20 @@ package com.nt2311.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExampleApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ExampleApplication.class, args);
+    var ctx = SpringApplication.run(ExampleApplication.class, args);
 
-    MyFirstClass myFirstClass = new MyFirstClass();
+    MyFirstClass myFirstClass = ctx.getBean(MyFirstClass.class);
     System.out.println(myFirstClass.sayHello());
+  }
+
+  @Bean
+  public MyFirstClass myFirstClass() {
+    return new MyFirstClass();
   }
 }
