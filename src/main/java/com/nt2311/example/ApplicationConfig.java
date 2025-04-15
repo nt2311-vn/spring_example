b96388e07ce66b2a9513cfd0fcf413c0fb5f3779
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-15 08:11:12 +0700

    when have two subclass in a single class use Qualifier with name to distinguise as dependency injection

--- src/main/java/com/nt2311/example/ApplicationConfig.java
+++ src/main/java/com/nt2311/example/ApplicationConfig.java
@@ -1,12 +1,20 @@
 package com.nt2311.example;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
   @Bean
-  public MyFirstClass myFirstClass() {
+  @Qualifier("bean1")
+  public MyFirstClass myFirstBean() {
     return new MyFirstClass("First bean");
+  }
+
+  @Bean
+  @Qualifier("bean2")
+  public MyFirstClass mySecondBean() {
+    return new MyFirstClass("Second bean");
   }
 }
--- src/main/java/com/nt2311/example/MyFirstService.java
+++ src/main/java/com/nt2311/example/MyFirstService.java
@@ -1,5 +1,6 @@
 package com.nt2311.example;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,7 +8,7 @@ public class MyFirstService {
 
   private MyFirstClass myFirstClass;
 
-  public MyFirstService(MyFirstClass myFirstClass) {
+  public MyFirstService(@Qualifier("bean2") MyFirstClass myFirstClass) {
     this.myFirstClass = myFirstClass;
   }
 
