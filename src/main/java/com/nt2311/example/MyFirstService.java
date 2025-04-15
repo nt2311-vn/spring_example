e0d80d70ae6c61ba50874773f077e290e6f4c3ad
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-15 08:28:48 +0700

    using Qualifier with method name to know which class to import

--- src/main/java/com/nt2311/example/MyFirstService.java
+++ src/main/java/com/nt2311/example/MyFirstService.java
@@ -1,15 +1,15 @@
 package com.nt2311.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
 
+  @Autowired
+  @Qualifier("mySecondBean")
   private MyFirstClass myFirstClass;
-
-  public MyFirstService(MyFirstClass myFirstClass) {
-    this.myFirstClass = myFirstClass;
-  }
 
   public String tellAStory() {
     return "the dependency is saying : " + myFirstClass.sayHello();
