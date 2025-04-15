650885efda597078f39c77673dd189372dc4c69a
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-15 09:08:00 +0700

    add property soruce classpath:filename to tell spring boot to scan custom property file

--- src/main/java/com/nt2311/example/ExampleApplication.java
+++ src/main/java/com/nt2311/example/ExampleApplication.java
@@ -11,5 +11,8 @@ public class ExampleApplication {
 
     MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
     System.out.println(myFirstService.tellAStory());
+    System.out.println(myFirstService.getCustomProperty());
+    System.out.println(myFirstService.getCustomPropertyInt());
+    System.out.println(myFirstService.getCustomPropertyFromAnotherFile());
   }
 }
--- src/main/java/com/nt2311/example/MyFirstService.java
+++ src/main/java/com/nt2311/example/MyFirstService.java
@@ -1,12 +1,24 @@
 package com.nt2311.example;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
+@PropertySource("classpath:custom.properties")
 public class MyFirstService {
 
   private final MyFirstClass myFirstClass;
+
+  @Value("Hello Toan")
+  private String customProperty;
+
+  @Value("${my.prop}")
+  private String customPropertyFromAnotherFile;
+
+  @Value("123")
+  private Integer customPropertyInt;
 
   public MyFirstService(@Qualifier("bean1") MyFirstClass myFirstClass) {
     this.myFirstClass = myFirstClass;
@@ -14,5 +26,17 @@ public class MyFirstService {
 
   public String tellAStory() {
     return "the dependency is saying : " + myFirstClass.sayHello();
+  }
+
+  public String getCustomProperty() {
+    return customProperty;
+  }
+
+  public String getCustomPropertyFromAnotherFile() {
+    return customPropertyFromAnotherFile;
+  }
+
+  public Integer getCustomPropertyInt() {
+    return customPropertyInt;
   }
 }
