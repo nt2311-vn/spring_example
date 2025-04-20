daa0799509e027365df54dc875f34f13de37ca7d
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-20 20:29:05 +0700

    clean the application test code for preparing rest http method

--- src/main/java/com/nt2311/example/ExampleApplication.java
+++ src/main/java/com/nt2311/example/ExampleApplication.java
@@ -1,6 +1,5 @@
 package com.nt2311.example;
 
-import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,15 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExampleApplication {
 
   public static void main(String[] args) {
-    var app = new SpringApplication(ExampleApplication.class);
-    app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
-    var ctx = app.run(args);
-
-    MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-    System.out.println(myFirstService.tellAStory());
-    System.out.println(myFirstService.getCustomProperty());
-    System.out.println(myFirstService.getCustomPropertyInt());
-    System.out.println(myFirstService.getCustomPropertyFromAnotherFile());
-    System.out.println(myFirstService.getCustomPropertyFromAnotherFile2());
+    SpringApplication.run(ExampleApplication.class);
   }
 }
