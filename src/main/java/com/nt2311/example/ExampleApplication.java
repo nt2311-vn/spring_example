5bd1bc9b50e8d93bbf6493b31cd1c1d3f2380190
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-20 20:07:10 +0700

    switch using set default properties

--- src/main/java/com/nt2311/example/ExampleApplication.java
+++ src/main/java/com/nt2311/example/ExampleApplication.java
@@ -9,7 +9,7 @@ public class ExampleApplication {
 
   public static void main(String[] args) {
     var app = new SpringApplication(ExampleApplication.class);
-    app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
+    app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "test"));
     var ctx = app.run(args);
 
     MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
