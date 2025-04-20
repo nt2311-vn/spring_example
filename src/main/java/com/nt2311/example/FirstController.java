6077eb10ca9fb8a7b993b7143bb4684f7418fecf
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-20 20:49:22 +0700

    create a custom response status map with httpstatus accepted

--- src/main/java/com/nt2311/example/FirstController.java
+++ src/main/java/com/nt2311/example/FirstController.java
@@ -1,6 +1,8 @@
 package com.nt2311.example;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +13,7 @@ public class FirstController {
   }
 
   @GetMapping("/hello-2")
+  @ResponseStatus(HttpStatus.ACCEPTED)
   public String sayHello2() {
     return "Hello 2 from my first controller";
   }
